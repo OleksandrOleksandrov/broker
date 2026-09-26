2bf83bf11aee943a139a5f7e9039bc67415d9a5a
Oleksandrov Oleksandr <aleksandrggggg@gmail.com>
2026-09-26 12:32:29 +0300

    increase memory size memory_size

--- terraform/main.tf
+++ terraform/main.tf
@@ -210,7 +210,7 @@ resource "aws_lambda_function" "api" {
   handler          = "lambda_handler.handler"
   runtime          = "python3.12"
   architectures    = ["arm64"]
-  memory_size      = 512
+  memory_size      = 1536
   timeout          = var.lambda_timeout
   tags             = local.common_tags
 
